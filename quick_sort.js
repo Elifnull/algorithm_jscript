ccc538e96cda50963527550a4216486801050de1
Serge Chvorun <elifnull@protonmail.com>
2022-02-19 23:20:50 -0500

    quick sort algo

--- quick_sort.js
+++ quick_sort.js
@@ -1,1 +1,34 @@
 //pivot point necessary
+
+function quickSort(arr, leftPos, rightPos, arrLength) {
+    let initialLeftPos = leftPos;
+    let initialRightPos = rightPos;
+    let direction = true;
+    let pivot = rightPos;
+    while ((leftPos - rightPos) < 0) {
+        if (direction) {
+        if (arr[pivot] < arr[leftPos]) {
+            quickSort.swap(arr, pivot, leftPos);
+            pivot = leftPos;
+            rightPos--;
+            direction = !direction;
+        } else
+            leftPos++;
+        } else {
+        if (arr[pivot] <= arr[rightPos]) {
+            rightPos--;
+        } else {
+            quickSort.swap(arr, pivot, rightPos);
+            leftPos++;
+            pivot = rightPos;
+            direction = !direction;
+        }
+        }
+    }
+    if (pivot - 1 > initialLeftPos) {
+        quickSort(arr, initialLeftPos, pivot - 1, arrLength);
+    }
+    if (pivot + 1 < initialRightPos) {
+        quickSort(arr, pivot + 1, initialRightPos, arrLength);
+    }
+}
