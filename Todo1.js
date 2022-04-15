11a137d1f013a375494ef5c1cde8f1f129deb9fa
Serge Chvorun <elifnull@protonmail.com>
2022-04-15 19:39:05 -0400

    refactoring of algos

--- Todo1.js
+++ Todo1.js
@@ -32,4 +32,30 @@ whileLoop = (count) => {
     console.log(`finished on count value ${count}`)
 }
 
-whileLoop(2000);
+// whileLoop(2000);
+
+birthdayTest1 = (num1,num2) =>{ //og test
+    if (num1 != num2){
+        if(num1 == 30 || 11){
+            if(num2 == 30 || 11){
+                console.log("Yep my birthday is 11/30")
+            }
+        }
+        
+    } else {
+        console.log("these are not my birthdates")
+    }
+}
+
+birthdayTest = (num1,num2) =>{
+    if (num1 != num2 && (num1 == 30 || 11) && (num2 == 30 || 11)){ //refactored!
+        console.log("Yep my birthday is 11/30")
+        
+    } else {
+        console.log("these are not my birthdates")
+    }
+}
+// birthdayTest(30,11);
+// birthdayTest(11,30);
+// birthdayTest(11,11);
+// birthdayTest(30,30);
