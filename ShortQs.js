0c27c6ca2c81d1c1310df4ac1bb4e5353db56943
Serge Chvorun <elifnull@protonmail.com>
2022-04-16 20:04:40 -0400

    Q?A sections added

---
+++ ShortQs.js
@@ -0,0 +1,21 @@
+/*
+1. it is a set of math functions and numbers that you can import into jscript.
+
+2. the reason for not having math built into jscript is to have  lighter version of it, 
+that does not contain those funcrtion,thus making it ligher to use.
+
+3. Math.radndom will produce a decimal between 0 and 1 (excluding the 1)\
+4. Math.floor will bring the float down to the closes integer, Math.ceil will bring the
+float to the nearest high integer, Math.trunc will make the float into an integer by cutting
+off the decimals. Math.round will round to the nearest integer.
+
+5.Math.trunct and Math.floor do not return the same values when the number domain is less than
+zero.
+
+6.% is the modulus operator, and it is useful to find the remainder, and determint the nature
+of numbers (odd, even);
+7. Variables declared outside of a function have a scope that is greater than the function so
+to access the variable you need to call it.
+
+8. commenting on code allows you to know what the block does without trying to figure it out.
+*/
