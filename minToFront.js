9da55f80413a2d5f7a7da74cd17c195da869bc03
Serge Chvorun <elifnull@protonmail.com>
2022-04-17 18:01:27 -0400

    async incrimemintation added

--- minToFront.js
+++ minToFront.js
@@ -1,5 +1,21 @@
 minToFront = (array) => {
-    let min = [];
-
+    let min = [array[0]];
+    let nuArray = [];
+    let i = 0;
+    for(let key in array){
+        if(array[key] < min[0]){
+            min[0] = array[key]
+            console.log(array[key])
+        } else{
+            console.log('else:',array[key])
+            nuArray[i] = array[key];
+            ++i;
+        }
+    }
+    let NuRay = [min[0],...nuArray]
+    console.log(NuRay);
+    console.log(min)
     
 }
+
+minToFront([2,3,4,6,1,0]);
