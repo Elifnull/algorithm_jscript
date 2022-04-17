514bede3a9bba30aba7a7fd0e2002c3bb93ce3c0
Serge Chvorun <elifnull@protonmail.com>
2022-04-16 20:16:52 -0400

    more Q/A added

--- ShortQs.js
+++ ShortQs.js
@@ -19,3 +19,36 @@ to access the variable you need to call it.
 
 8. commenting on code allows you to know what the block does without trying to figure it out.
 */
+
+/*
+1. source code is the sum total of all the functions, variables, instructions, loops within a
+particular appliction. the source code is the code of the program.
+
+2. computers are smart because they are capable of doing many functions over a short period of 
+time.
+
+3.the purpose of a programming lanugage is to interface between the programmer and the computer.
+it is a medium that allows the programmer to abstact out of asembly code and binary into something
+more understandable. then this code is translated into binary for the computer to work it out.
+
+4.Python, javascript, java, rust, C# these are all programming languages. there are many languages
+becase each language does something useful that the others don't do.
+
+5.a variable is a container for data. it is useful becaue it formalizes and shorthands those data.
+
+6.one sets the value of variable and the other checks the equivilence between the two variables.
+
+7.the difference is that one is a strick equivelence, and the other is a loose equivilence. the
+strick equivilence compares values and types.
+
+8.developer console exists in order for the developer to test out the program, and track the flow
+of data.
+
+9.a conditional statment is a true/false statment, where those are the only two options. upon a true
+value the program will execute the code encapsulated within the boolean.
+
+10.while and for loops allows us to decrease the amount of repeated code by setting the parameters
+and conditions under which the code will be executed.
+
+11.
+*/
